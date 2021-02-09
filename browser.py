1a583b57d3c85182878854c4497c0c9d02e64902
Kabiirk <kabiirk@gmail.com>
2021-02-09 08:36:28 -0800

    Minor Bug Fixes

--- browser.py
+++ browser.py
@@ -7,17 +7,17 @@ class MyApp(wx.App):
         self.InitBrowser()
 
     def InitBrowser(self):
-        webbrowser = WebFrame(None, "WxBrowser", pos=(100, 100))
+        webbrowser = WebFrame(None, 'WxBrowser', pos=(100, 100))
         webbrowser.Show()
 
 class WebFrame(wx.Frame):
-    def __init__(self, title, parent, pos):
+    def __init__(self, parent, title, pos):
         super().__init__(parent, title=title, pos=pos)
 
         # Create Variables
         # Browser
         self._browser = html2.WebView.New(self)
-        self._browser.LoadURL("https://kabiirk.github.io/")
+        self._browser.LoadURL("www.google.com")
 
         # Navbar
         self._navbar = NavBar(self, self._browser)
@@ -27,7 +27,7 @@ class WebFrame(wx.Frame):
         sizer.Add(self._navbar, 0, wx.EXPAND)
         sizer.Add(self._browser, 1, wx.EXPAND)
         self.SetSizer(sizer)
-        # The Above lines define layout as
+        # The Above lines define the Frame layout as
         #  _________________________________________
         # | (<-) (->) | https://kabiirk.github.io   |
         # |___________|_____________________________|
@@ -39,7 +39,7 @@ class WebFrame(wx.Frame):
 
 
 class NavBar(wx.Panel):
-    # added browser Parameter so that Navbar could communcate with the Browser
+    # added browser Parameter so that Navbar could communicate with the Browser
     def __init__(self, parent, browser):
         super().__init__(parent)
 
@@ -49,17 +49,20 @@ class NavBar(wx.Panel):
 
         # Buttons
         back = wx.Button(self, style=wx.BU_EXACTFIT)
-        back.Bitmap = wx.ArtProvider.BetBitmap(wx.ART_GO_BACK, wx.ART_TOOLBAR)
+        back.Bitmap = wx.ArtProvider.GetBitmap(wx.ART_GO_BACK, wx.ART_TOOLBAR)
 
         fwd = wx.Button(self, style=wx.BU_EXACTFIT)
-        fwd.Bitmap = wx.ArtProvider.BetBitmap(wx.ART_GO_FORWARD, wx.ART_TOOLBAR)
+        fwd.Bitmap = wx.ArtProvider.GetBitmap(wx.ART_GO_FORWARD, wx.ART_TOOLBAR)
 
         sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.add(back, 0, wx.ALL, 5)
-        sizer.add(fwd, 0, wx.ALL, 5)
-        sizer.add(self._url, 0, wx.ALL, 5)
-
+        sizer.Add(back, 0, wx.ALL, 5)
+        sizer.Add(fwd, 0, wx.ALL, 5)
+        sizer.Add(self._url, 0, wx.ALL, 5)
         self.SetSizer(sizer)
+        # The Above lines define Navigation Bar layout as
+        #  _________________________________________
+        # | (<-) (->) | https://kabiirk.github.io   |
+        # |___________|_____________________________|
 
     def OnEnter(self, event):
         self.browser.LoadURL(self._url.Value)
@@ -72,6 +75,6 @@ class NavBar(wx.Panel):
         event.Enable(self.browser.CanGoForward())
         self._browser.GoForward()
 
-if __name__=="main":
+if __name__=="__main__":
     app = MyApp()
     app.MainLoop()
